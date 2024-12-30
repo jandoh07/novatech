392c4ff83c37c629a0e63fa8432215c1f7e1831c
Joshua <amjoshua59@gmail.com>
2024-12-30 00:32:20 +0000

    React setup with tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Dashboard from "./pages/Dashboard";
+import SearchResults from "./pages/SearchResults";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ProductProvider } from "./context/ProductProvider";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProductProvider>
+        <BrowserRouter>
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<div>404</div>} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ProductProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
