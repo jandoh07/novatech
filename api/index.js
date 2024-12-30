bb660e21b5b6142e5c29e2540924213b62e62c93
Joshua <amjoshua59@gmail.com>
2024-12-30 00:03:18 +0000

    Api setup with dependencies and basic structure

---
+++ api/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import wishlistRoutes from "./routes/wishlistRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import s3Routes from "./routes/s3Routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+app.use("/wishlist", wishlistRoutes);
+app.use("/search", searchRoutes);
+app.use("/s3", s3Routes);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log(`Connected to MongoDB`))
+  .catch((err) => console.log(err));
+
+app.listen(`3000`, () => {
+  console.log(`Server is running on port 3000`);
+});
