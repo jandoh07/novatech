bc98c6419fb934cf0040b184e8f4e34904a23d77
Joshua <amjoshua59@gmail.com>
2025-01-03 02:09:49 +0000

    feat: add functionality to get cart products from an array of ids

--- api/routes/cartRoutes.js
+++ api/routes/cartRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   addToCart,
   clearCart,
+  getCartProducts,
   removeFromCart,
 } from "../controllers/cartController.js";
 import verifyJwt from "../middleware/verifyJwt.js";
@@ -9,6 +10,7 @@ import verifyJwt from "../middleware/verifyJwt.js";
 const router = express.Router();
 
 router.post("/:id", verifyJwt, addToCart);
+router.post("/", verifyJwt, getCartProducts);
 router.delete("/:id", verifyJwt, removeFromCart);
 router.delete("/", verifyJwt, clearCart);
 
