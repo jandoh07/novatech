d0594865ecc0c74fcc291d633496f83f551cfab7
Joshua <amjoshua59@gmail.com>
2025-01-04 01:48:24 +0000

    update: search api endpoint

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -1,12 +1,16 @@
 import Product from "../models/productModel.js";
 
 export const search = async (req, res) => {
+  const { query } = req.params;
+  const { limit } = req.query;
+
   try {
-    const { query } = req.params;
-    const results = await Product.find(
-      { $text: { $search: query } },
-      { score: { $meta: "textScore" } }
-    ).sort({ score: { $meta: "textScore" } });
+    const results = await Product.find({
+      $or: [
+        { name: { $regex: query, $options: "i" } },
+        { category: { $regex: query, $options: "i" } },
+      ],
+    }).limit(parseInt(limit));
 
     res.status(200).json(results);
   } catch (error) {
