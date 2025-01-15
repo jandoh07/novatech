7f374d5970171eb89ddc62fc58296eb885208c78
Joshua <amjoshua59@gmail.com>
2025-01-15 15:23:20 +0000

    refactor: remove unused getProductsFromIds route from productRoutes

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -4,7 +4,6 @@ import {
   deleteProduct,
   getProductById,
   getProducts,
-  getProductsFromIds,
   getSearchSuggestions,
   search,
   updateProduct,
@@ -15,9 +14,6 @@ const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/:ids", getProductById);
-// router.get("/ids", async (req, res) => {
-//   res.json({ message: "This route is not implemented yet" });
-// });
 router.get("/search/:query", search);
 router.get("/search/suggestions/:query", getSearchSuggestions);
 router.post("/", verifyJwt, createProduct);
