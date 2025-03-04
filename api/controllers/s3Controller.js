e5c34603cead3f7f2ed59dadd48a1e9cc0ba817d
Joshua <amjoshua59@gmail.com>
2025-03-04 16:34:12 +0000

    chore: change aws access key variable name

--- api/controllers/s3Controller.js
+++ api/controllers/s3Controller.js
@@ -13,7 +13,7 @@ export const generatePresignedUrl = async (req, res) => {
   const client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
   });
